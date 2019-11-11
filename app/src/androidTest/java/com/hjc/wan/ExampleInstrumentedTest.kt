373d81498663fe5f6b53342418bf3a857b168daa
hjcasd <2959383735@qq.com>
2019-11-11 18:34:30 +0800

    去除报错信息

--- app/src/androidTest/java/com/hjc/wan/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hjc/wan/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
 package com.hjc.wan
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hjc.gif", appContext.packageName)
+        assertEquals("com.hjc.wan", appContext.packageName)
     }
 }
