e55b18dfb4d7af998a1f9fdfe8855c998dcc79ee
HJC <2959383735@qq.com>
2020-12-28 19:40:19 +0800

    v1分支创建

--- app/src/main/java/com/hjc/wan/ui/collect/CollectActivity.kt
+++ app/src/main/java/com/hjc/wan/ui/collect/CollectActivity.kt
@@ -25,7 +25,6 @@ import java.util.*
 class CollectActivity : BaseMvpActivity<CollectContract.View, CollectPresenter>(),
     CollectContract.View {
 
-
     override fun createPresenter(): CollectPresenter {
         return CollectPresenter()
     }
@@ -38,7 +37,6 @@ class CollectActivity : BaseMvpActivity<CollectContract.View, CollectPresenter>(
     override fun getLayoutId(): Int {
         return R.layout.activity_collect
     }
-
 
     override fun initView() {
         super.initView()
